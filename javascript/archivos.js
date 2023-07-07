eecdfe78ed0161471f7f527ae4a416e33029e416
Carlos Julian Lopez <milkanojanich@gmail.com>
2023-07-07 12:42:13 -0300

    cambios

--- javascript/archivos.js
+++ javascript/archivos.js
@@ -128,7 +128,7 @@ if(ancho < 400 && ancho > 230){
     foto.style.height="15vh"
     foto.style.width="42%"
     foto.style.top="10vh"
-    tem.style.marginTop="10vh"
+    tem.style.marginTop="12vh"
     max.style.marginTop="-3.8vh"
     temperatura.style.marginTop="-3vh"
 }
